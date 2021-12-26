13220fa5e1cb16c0171795beba874c0eb983853f
Cristian Franco <cfrancobedoya@gmail.com>
2021-12-26 17:36:58 -0500

    Add react navigation

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
+import React from 'react'
+import 'react-native-gesture-handler';
+
 import { StyleSheet, Text, View } from 'react-native';
+
 import Greating from './src/components/Greating';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  const names = [{firstName: 'Cristian', lastName: 'Franco'}, { firstName: 'Alejandra', lastName: 'Bertón'}, { firstName: 'Alejandro'}];
+  const names = [{firstName: 'Alejandra', lastName: 'Franco'}, { firstName: 'Alejandra', lastName: 'Bertón'}, { firstName: 'Alejandro'}];
   return (
-    <View style={styles.container}>
-      {names.map((person) => {
-        return (<Greating firstName={person.firstName} lastName={person.lastName} key={Math.random()}/>)
-      })}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        {names.map((person) => {
+          return (<Greating firstName={person.firstName} lastName={person.lastName} key={Math.random()}/>)
+        })}
+      </View>
+    </NavigationContainer>
   );
 }
 
