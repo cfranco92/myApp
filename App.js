8ef6e2332d2298ff49848dfcdd0bca6f476ca784
Cristian Franco <cfrancobedoya@gmail.com>
2021-12-26 18:59:12 -0500

    Add Drawer Navigation

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
-import NavigationStack from './src/navigation/NavigationStack';
-import NavigationTab from './src/navigation/NavigationTab';
+// import NavigationStack from './src/navigation/NavigationStack';
+// import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
   const names = [{firstName: 'Alejandra', lastName: 'Franco'}, { firstName: 'Alejandra', lastName: 'Bertón'}, { firstName: 'Alejandro'}];
   return (
     <NavigationContainer>
       {/* <NavigationStack/> */}
-      <NavigationTab/>
+      {/* <NavigationTab/> */}
+      <NavigationDrawer/>
     </NavigationContainer>
   );
 }
